661daaf045f239969dfdb4efedab64ac8020426f
Istvan Richter <irichter@uplandsoftware.com>
2018-02-04 15:17:54 +0100

    Setup frontend placeholders

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { ServiceModule } from './service/service.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeModule } from './page/home/home.module';
+import { LoginModule } from './page/login/login.module';
 
 
 @NgModule({
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    routing,
+    ServiceModule,
+    HomeModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
