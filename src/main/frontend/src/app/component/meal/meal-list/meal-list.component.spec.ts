ee9cf514cccd2e23b85e078ad10dede373c00934
Istvan Richter <irichter@uplandsoftware.com>
2018-02-07 17:13:07 +0100

    Add meaningful tests

--- src/main/frontend/src/app/component/meal/meal-list/meal-list.component.spec.ts
+++ src/main/frontend/src/app/component/meal/meal-list/meal-list.component.spec.ts
@@ -1,25 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MealListComponent } from './meal-list.component';
+import { Meal } from '../../../domain/meal';
 
 describe('MealListComponent', () => {
   let component: MealListComponent;
-  let fixture: ComponentFixture<MealListComponent>;
+  const meals: Meal[] = [
+    {id: 1, date: '2018-01-01', time: '12:00:00', calories: 1000, description: 'Lunch'},
+    {id: 2, date: '2018-01-01', time: '18:00:00', calories: 100, description: 'Dinner'},
+    {id: 3, date: '2018-01-01', time: null, calories: 10, description: 'Breakfast'},
+    {id: 4, date: null, time: '12:00:00', calories: 1000, description: 'Lunch'},
+    {id: 5, date: null, time: '18:00:00', calories: 100, description: 'Dinner'},
+    {id: 6, date: null, time: null, calories: 10, description: 'Breakfast'}
+  ];
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MealListComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MealListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach((done: any) => {
+    component = new MealListComponent();
+    done();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should sort meals', () => {
+    component.meals = meals;
+    expect(component.meals.map(meal => meal.id)).toEqual([6, 4, 5, 3, 1, 2]);
   });
+
+  beforeEach((done: any) => {
+    component = new MealListComponent();
+    done();
+  });
+
+  it('should return correct group indices', () => {
+    component.meals = meals;
+    expect(component.group(null).index).toBe(0);
+    expect(component.group('2018-01-01').index).toBe(3);
+  });
+
+  it('should return correct group sizes', () => {
+    component.meals = meals;
+    expect(component.group(null).size).toBe(3);
+    expect(component.group('2018-01-01').size).toBe(3);
+  });
+
+  it('should return correct sum of calories', () => {
+    component.meals = meals;
+    expect(component.group(null).calories).toBe(1110);
+    expect(component.group('2018-01-01').calories).toBe(1110);
+  });
+
 });
